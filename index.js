d4671f8ee4bdc0bf75da6b4392729a5ecdb0745a
HWN <heatherwensler@gmail.com>
2017-08-23 14:01:36 -0700

    added json parser

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ const request = require('request');
 
 //set ports - environment variable OR localhost 5000
 app.set('port', (process.env.PORT || 5000))
+
+//use json parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
