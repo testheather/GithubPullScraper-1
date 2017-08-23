ed9a7556849da32c42cce2375cf37d66dd4ed65b
HWN <heatherwensler@gmail.com>
2017-08-23 14:00:56 -0700

    added npm packages

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  * 08/23/2017
  */
 
+//npm packages
+const bodyParser = require('body-parser')
+const request = require('request');
 
 //set ports - environment variable OR localhost 5000
 app.set('port', (process.env.PORT || 5000))
