fc475e7f10627c2c6cdb41479dda2c884699c6a3
HWN <heatherwensler@gmail.com>
2017-08-23 14:15:41 -0700

    using express, strict, and nodeJS 6

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
  * 08/23/2017
  */
 
+"use strict";
+
 //npm packages
 const bodyParser = require('body-parser');
 const request = require('request');
+const express = require('express');
+const app = express();
 
 //set ports - environment variable OR localhost 5000
 app.set('port', (process.env.PORT || 5000));
@@ -13,4 +17,5 @@ app.set('port', (process.env.PORT || 5000));
 //use json parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 
