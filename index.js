7c2bbbb7cc134849a26bd9cdb06130100a44d166
HWN <heatherwensler@gmail.com>
2017-08-23 14:06:57 -0700

    added ; like an adult

--- index.js
+++ index.js
@@ -4,12 +4,13 @@
  */
 
 //npm packages
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const request = require('request');
 
 //set ports - environment variable OR localhost 5000
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 //use json parser
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
