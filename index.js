5c29ef32e887c858aa3ef16274dda27d4814c115
HWN <heatherwensler@gmail.com>
2017-08-28 08:48:35 -0700

    pass all data to received function

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/pull', function(req, res){
 
     try{
         console.log('A pull event happened!');
-        pullEventReceived(data.entry);
+        pullEventReceived(data);
     }
     catch (e) {
         console.log("Webhook received unknown event: ", e);
